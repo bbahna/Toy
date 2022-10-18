e1591961d791c67294510c21f9681bb2915253ef
bbahna <moondomi_@naver.com>
2022-10-18 11:11:44 +0900

    :zap: Add router v6 redirect +replace: history 이력X (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/login';
 import ListPage from './pages/list';
 import DetailPage from './pages/detail';
@@ -12,6 +12,8 @@ function App() {
         <Route path="/list" element={<ListPage/>} />
         <Route path="/detail" element={<DetailPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
+        {/* v6 redirect 설정 +replace: history 이력X */}
+        <Route path="/" element={<Navigate replace to="/login" />} />
       </Routes>
 		</BrowserRouter>
   );
